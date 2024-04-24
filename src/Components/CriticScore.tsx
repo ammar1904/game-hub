8babf6d0a9a25000648c22b3a22bf30a7a7db652
Ammar Khan <ammarkhan1904@gmail.com>
2024-04-24 21:58:02 +0500

    Highlight the selected genre.

--- src/Components/CriticScore.tsx
+++ src/Components/CriticScore.tsx
@@ -6,7 +6,6 @@ interface Props {
 
 const CriticScore = ({ score }: Props) => {
   let color = score > 75 ? "green" : score > 60 ? "yellow" : "";
-  console.log(color);
 
   return (
     <Badge colorScheme={color} fontSize="14px" paddingX={2} borderRadius="4px">
