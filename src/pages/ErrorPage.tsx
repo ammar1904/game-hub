d46a664a8ea4db5d110da196faa1eda0ea4b3d66
Ammar Khan <ammarkhan1904@gmail.com>
2024-05-15 15:57:31 +0500

    Setup routing

---
+++ src/pages/ErrorPage.tsx
@@ -0,0 +1,16 @@
+import { isRouteErrorResponse, useRouteError } from "react-router-dom";
+
+const ErrorPage = () => {
+  const error = useRouteError();
+
+  return (
+    <>
+      <h1>Oops...</h1>
+      <p>
+        {isRouteErrorResponse(error) ? "Invalid Route" : "Unexpected Error"}
+      </p>
+    </>
+  );
+};
+
+export default ErrorPage;
