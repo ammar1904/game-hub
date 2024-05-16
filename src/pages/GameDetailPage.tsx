9fcbe4cda4b79bde5b82563f090ea1355938756c
Ammar Khan <ammarkhan1904@gmail.com>
2024-05-16 17:03:43 +0500

    adding game attributes.

--- src/pages/GameDetailPage.tsx
+++ src/pages/GameDetailPage.tsx
@@ -1,22 +1,29 @@
 import { useParams } from "react-router-dom";
 import useGame from "../hooks/useGame";
-import { Button, Heading, Spinner, Text } from "@chakra-ui/react";
-import { useState } from "react";
+import { Box, Grid, Heading, Spinner } from "@chakra-ui/react";
 import ExpandableText from "../components/ExpandableText";
+import GameAttribute from "../components/GameAttribute";
 
 const GameDetailPage = () => {
   const { slug } = useParams();
   const { data: game, isLoading, error } = useGame(slug!);
+  console.log(game);
 
   if (isLoading) return <Spinner />;
 
   if (error || !game) throw error;
 
   return (
-    <>
+    <Box padding="20px">
       <Heading>{game?.name}</Heading>
       <ExpandableText>{game?.description_raw}</ExpandableText>
-    </>
+      <Box>
+        <GameAttribute
+          title="Platforms"
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
+      </Box>
+    </Box>
   );
 };
 
