cbb3eee7545916bcd86d14aed312af79808b6075
Ammar Khan <ammarkhan1904@gmail.com>
2024-05-21 15:07:54 +0500

    Fetching game trailers.

--- src/Components/GameTrailer.tsx
+++ src/Components/GameTrailer.tsx
@@ -1,6 +1,14 @@
 import React from "react";
+import useTrailers from "../hooks/useTrailers";
 
-const GameTrailer = () => {
+interface Props {
+  gameId: number;
+}
+
+const GameTrailer = ({ gameId }: Props) => {
+  const { data, error, isLoading } = useTrailers(gameId);
+  console.log(data);
+
   return <div>GameTrailer</div>;
 };
 
