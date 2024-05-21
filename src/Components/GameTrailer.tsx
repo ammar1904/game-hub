16855c4cc6ae092fa5f0f9d60993129d5b250549
Ammar Khan <ammarkhan1904@gmail.com>
2024-05-21 15:28:01 +0500

    fetching higher res trailer.

--- src/Components/GameTrailer.tsx
+++ src/Components/GameTrailer.tsx
@@ -7,6 +7,7 @@ interface Props {
 
 const GameTrailer = ({ gameId }: Props) => {
   const { data, error, isLoading } = useTrailers(gameId);
+  console.log(data);
 
   if (isLoading) return null;
 
@@ -15,7 +16,7 @@ const GameTrailer = ({ gameId }: Props) => {
   const first = data?.results[0];
 
   return first ? (
-    <video src={first.data[480]} poster={first.preview} controls />
+    <video src={first.data["max"]} poster={first.preview} controls />
   ) : null;
 };
 
